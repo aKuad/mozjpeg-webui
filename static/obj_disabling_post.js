d44b725510445ffff2f1fe08ec4f308cf159b113
aKuad <53811809+aKuad@users.noreply.github.com>
2022-12-18 09:16:03 +0900

    Argument type error (must be objects) added

--- static/obj_disabling_post.js
+++ static/obj_disabling_post.js
@@ -9,6 +9,11 @@
  * @author aKuad
  */
  async function obj_disabling_post(url, body = undefined, objs = []) {
+  // Check are 'objs' elements object
+  if(!check_are_objects(objs)) {
+    throw new Error("Non object element(s) specified as argument.");
+  }
+
   // Disable all objects
   for(const obj of objs) {
     obj.disabled = true;
@@ -23,4 +28,18 @@
   }
 
   return response
+}
+
+
+/**
+ * Return are input array's all elements object
+ *
+ * @param {Array<*>} objs Objects to check containing in array
+ * @returns {boolean} All elements are object: true, other cases: false
+ */
+ function check_are_objects(objs) {
+  let is_obj = objs.map(obj => typeof obj === "object");
+  // Exist 'false' means: non-object element(s) exist
+  // Only 'true' means  : all element(s) are object
+  return !is_obj.includes(false);
 }
