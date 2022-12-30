589489bd2d55c997337f8e8ad606b93771f912b1
aKuad <53811809+aKuad@users.noreply.github.com>
2022-12-30 14:16:36 +0900

    Multiple file optimizing function added

--- util/jpeg_opt.py
+++ util/jpeg_opt.py
@@ -38,3 +38,34 @@ def jpeg_opt(jpeg_in: bytes, executable: str = "jpegtran", options: str = "-opti
     raise ValueError("Input was invalid JPEG binary")
 
   return res.stdout
+
+
+def jpeg_opt_batch(jpegs_in: "list[bytes]", executable: str = "jpegtran", options: str = "-optimize -copy all") -> "list[bytes]":
+  """JPEG optimizing by external command, for multiple files
+
+  Args:
+    jpeg_in (list[bytes]): List of target JPEG binary items
+    executable (str): Executable `jpegtran` path or command
+    options (str): Options to pass executable
+
+  Returns:
+    list[bytes]: Optimized JPEG binary items
+
+  Raises:
+    OSError: Specified executable or command not found
+    ValueError: Input was invalid JPEG binary
+
+  Note:
+    It requires `jpegtran` executable.
+    Put executable into $PATH directory as ``jpegtran``
+      or specify executable path to argument.
+
+  Author:
+    aKuad
+
+  """
+  jpegs_out = []
+  for jpeg_in in jpegs_in:
+    jpegs_out.append(jpeg_opt(jpeg_in, executable, options))
+
+  return jpegs_out
