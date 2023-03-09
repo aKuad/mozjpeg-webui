53fcf155e51eb70e4e28ab87d990efd31cfbb956
aKuad <53811809+aKuad@users.noreply.github.com>
2023-03-09 22:37:39 +0900

    Messages, requirements checking refactoring

--- systemd-setup.sh
+++ systemd-setup.sh
@@ -4,41 +4,34 @@ SRC_DIR="/opt/mozjpeg-webui/"
 SERVICE_DIR="/etc/systemd/system/"
 
 # Requirements checking section
-## Is root user
 if [ `whoami` != "root" ]; then
   echo "Root permission required." > /dev/stderr
   exit 1
 fi
 
-## Is systemd available
+is_installable=1
+
 systemctl --version 1>/dev/null 2>/dev/null
-stat=$?
-if [ "$stat" != 0 ]; then
-  echo "'systemd' is unavailable in this system." > /dev/stderr
-  exit 1
+if [ "$?" != 0 ]; then
+  echo "systemd is unavailable in this system." > /dev/stderr
+  is_installable=0
 fi
 
-## Is mozjpeg installed
 /opt/mozjpeg/bin/jpegtran -version 1>/dev/null 2>/dev/null
-stat=$?
-if [ "$stat" != 0 ]; then
-  echo "'mozjpeg' is not installed in this system." > /dev/stderr
-  exit 1
+if [ "$?" != 0 ]; then
+  echo "mozjpeg is unavailable in this system." > /dev/stderr
+  is_installable=0
 fi
 
-## Is python3 available
-py_ver=`python3 -V 2>/dev/null`
-stat=$?
-if [ "$stat" != 0 ]; then
-  echo "'python3' is unavailable in this system." > /dev/stderr
-  exit 1
+python3 -V 1>/dev/null 2>/dev/null
+if [ "$?" != 0 ]; then
+  echo "python3 is unavailable in this system." > /dev/stderr
+  is_installable=0
 fi
 
-## Extract python version
-py_ver=${py_ver#* }
-py_ver=${py_ver%.*}
-py_ver_maj=${py_ver%.*}
-py_ver_min=${py_ver#*.}
+if [ "$is_installable" != 1 ]; then
+  exit 1
+fi
 
 
 # Setup section
@@ -46,33 +39,37 @@ py_ver_min=${py_ver#*.}
 if [ -e $SRC_DIR ]; then
   echo "Mozjpeg-webui already installed."
   echo -n "Will you reinstall? [Y/n]: "
-  # Overwriting install or abort
   read uin
-  uin=`echo -n $uin | tr 'A-Z' 'a-z'` # Convert to lowercase
-  if [ $uin != "y" ] && [ $uin != "yes" ]; then
+  if [ `echo -n $uin | tr 'A-Z' 'a-z'` != "y" ]; then   # Converting lowercase
     echo "Aborting" > /dev/stderr
     exit 1
   fi
+  # Overwriting install
   rm -r $SRC_DIR
   rm -f "$SERVICE_DIR/mozjpeg-webui.service"
 fi
 
 ## Try to create python environment
-mkdir /opt/mozjpeg-webui
+mkdir $SRC_DIR
+echo "Creating python virtual environment..."
 python3 -m venv "$SRC_DIR/.venv/"
-stat=$?
-if [ "$stat"!= 0 ]; then
-  # Red color message for fatal errors
-  echo "\033[0;31mFailed to create python virtual environment.\033[0m" > /dev/stderr
+if [ "$?"!= 0 ]; then
+  echo "\033[0;31mFailed to create python virtual environment.\033[0m" > /dev/stderr  # Red color
+  rm -rf $SRC_DIR
   exit 1
 fi
 $SRC_DIR/.venv/bin/pip install -r requirements.txt
+
+## Extract python minor version
+py_ver=`python3 -V 2>/dev/null`
+py_ver=${py_ver%.*}
+py_ver=${py_ver##*.}
 
 ## Copy sources
 cp -r src/pages $SRC_DIR
 cp -r src/static $SRC_DIR
 cp -r src/util $SRC_DIR
-if [ "$py_ver_min" -lt 9 ]; then
+if [ "$py_ver" -lt 9 ]; then
   cp src/main_3-8.py $SRC_DIR
   cat mozjpeg-webui.service | sed -e "s/main_3-9.py/main_3-8.py/" > $SERVICE_DIR/mozjpeg-webui.service
 else
@@ -85,12 +82,12 @@ systemctl daemon-reload
 
 
 # Finish section
-echo "\033[37;42;1mSuccess to setup.\033[0m"
+echo "\033[37;42;1mSuccess to setup.\033[0m"  # Green color
 cat << EOF
 Now you can start sevice with:
   sudo systemctl start mozjpeg-webui
 
-  and access to http://localhost:8000/
+App will be available on: http://localhost:8000/
 
 If you want enable auto start:
   sudo systemctl enable mozjpeg-webui
